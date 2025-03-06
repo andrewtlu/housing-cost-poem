962f821255959b39cf9dfb93d41bf45a391f8fba
andrewtlu <andrew.lu@emory.edu>
2025-03-06 11:16:17 -0500

    Fix prettier styling

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,45 +1,45 @@
 <script lang="ts">
-	import '../app.css';
+    import "../app.css";
 </script>
 
 <div class="h-screen snap-y snap-mandatory overflow-y-scroll">
-	<div class="flex h-screen snap-center items-center justify-center" id="title">
-		<h1>Affording an Abode: Affordable Housing in Urban America</h1>
-	</div>
+    <div class="flex h-screen snap-center items-center justify-center" id="title">
+        <h1>Affording an Abode: Affordable Housing in Urban America</h1>
+    </div>
 
-	<div class="flex h-screen snap-center items-center justify-center" id="verse1">
-		<ul>
-			<li>Home—yes, four walls to keep warm</li>
-			<li>and safe refuge in one’s room—</li>
-			<li>a haven for kin to form</li>
-			<li>and our childhood dreams to bloom:</li>
-		</ul>
-	</div>
+    <div class="flex h-screen snap-center items-center justify-center" id="verse1">
+        <ul>
+            <li>Home—yes, four walls to keep warm</li>
+            <li>and safe refuge in one’s room—</li>
+            <li>a haven for kin to form</li>
+            <li>and our childhood dreams to bloom:</li>
+        </ul>
+    </div>
 
-	<div class="flex h-screen snap-center items-center justify-center" id="verse2">
-		<ul>
-			<li>Plans of school, moving downtown</li>
-			<li>cut short. Finding no space within,</li>
-			<li>they plea: “Just earn your pension!</li>
-			<li>For what? To be kept trapped therein?</li>
-		</ul>
-	</div>
+    <div class="flex h-screen snap-center items-center justify-center" id="verse2">
+        <ul>
+            <li>Plans of school, moving downtown</li>
+            <li>cut short. Finding no space within,</li>
+            <li>they plea: “Just earn your pension!</li>
+            <li>For what? To be kept trapped therein?</li>
+        </ul>
+    </div>
 
-	<div class="flex h-screen snap-center items-center justify-center" id="verse3">
-		<ul>
-			<li>First, redlining carved us up;</li>
-			<li>now, though, housing builds gaps—</li>
-			<li>funny—our fathers’ missteps</li>
-			<li>make funding fortune perhaps…</li>
-		</ul>
-	</div>
+    <div class="flex h-screen snap-center items-center justify-center" id="verse3">
+        <ul>
+            <li>First, redlining carved us up;</li>
+            <li>now, though, housing builds gaps—</li>
+            <li>funny—our fathers’ missteps</li>
+            <li>make funding fortune perhaps…</li>
+        </ul>
+    </div>
 
-	<div class="flex h-screen snap-center items-center justify-center" id="verse4">
-		<ul>
-			<li>…hopeless. Yet, hopes still remain.</li>
-			<li>Though fate curses us to roam,</li>
-			<li>shouldn’t we defy this harm,</li>
-			<li>make plausible a path home?</li>
-		</ul>
-	</div>
+    <div class="flex h-screen snap-center items-center justify-center" id="verse4">
+        <ul>
+            <li>…hopeless. Yet, hopes still remain.</li>
+            <li>Though fate curses us to roam,</li>
+            <li>shouldn’t we defy this harm,</li>
+            <li>make plausible a path home?</li>
+        </ul>
+    </div>
 </div>
