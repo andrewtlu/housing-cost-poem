64cc3f60d9f67b8bd12872dce2b2e7563a2d7503
andrewtlu <andrew.lu@emory.edu>
2025-03-06 21:09:17 -0500

    Separate parallax background into separate component

---
+++ website/src/components/parallax.svelte
@@ -0,0 +1,46 @@
+<!-- @component
+Parallax component, used to render the city skyline in the background of the poem
+ -->
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    /** DOM element of scroll */
+    let { scrollContainer }: { scrollContainer: Element } = $props();
+    /** The current scroll position */
+    let scrollLeft = $state(0);
+    /** The maximum scroll position */
+    let maxScroll = $state(1);
+
+    /** Update scroll position */
+    const handleScroll = () => {
+        scrollLeft = scrollContainer.scrollLeft;
+    };
+
+    /** Update the maximum scroll position */
+    const updateMaxScroll = () => {
+        maxScroll = scrollContainer.scrollWidth - scrollContainer.clientWidth;
+    };
+
+    onMount(() => {
+        scrollContainer.addEventListener("scroll", handleScroll);
+        window.addEventListener("resize", updateMaxScroll);
+
+        updateMaxScroll();
+
+        return () => {
+            // clean listeners when component is unmounted
+            scrollContainer.removeEventListener("scroll", handleScroll);
+            window.removeEventListener("resize", updateMaxScroll);
+        };
+    });
+</script>
+
+<div class="absolute top-0 -z-50 h-screen w-screen overflow-hidden">
+    <!-- trust me the math works -->
+    <img
+        src="/city.png"
+        alt="background parallax city"
+        class="scale-200 absolute bottom-0 left-0 -translate-y-1/2"
+        style={`transform: translateX(${25 - (scrollLeft / maxScroll) * 50}%)`}
+    />
+</div>
--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,50 +1,23 @@
 <script lang="ts">
-    import { onMount } from "svelte";
     import "../app.css";
     import Verse1 from "../components/pages/verse1.svelte";
     import Verse2 from "../components/pages/verse2.svelte";
     import Verse3 from "../components/pages/verse3.svelte";
     import Verse4 from "../components/pages/verse4.svelte";
+    import Parallax from "../components/parallax.svelte";
 
-    /** DOM element of scroll */
-    let content: Element;
-    let scrollLeft = 0,
-        maxScroll = 1;
+    let content: Element; // for parallax component
+
     let verses = [Verse1, Verse2, Verse3, Verse4];
     let keyframe = 0;
 
     // TODO: fix keyframing (tie to scroll position and pass to verse as prop)
     // TODO: add scroll (button) bar
-
-    const handleScroll = () => {
-        scrollLeft = content.scrollLeft;
-        console.log(-(scrollLeft / maxScroll) * 100);
-    };
-
-    const updateMaxScroll = () => {
-        maxScroll = content.scrollWidth - content.clientWidth;
-    };
-
-    onMount(() => {
-        updateMaxScroll();
-        content.addEventListener("scroll", handleScroll);
-        window.addEventListener("resize", updateMaxScroll);
-        return () => {
-            // make sure listeners are cleaned up
-            content.removeEventListener("scroll", handleScroll);
-            window.removeEventListener("resize", updateMaxScroll);
-        };
-    });
 </script>
 
-<div class="absolute -z-50 h-screen w-screen overflow-hidden">
-    <img
-        src="/city.png"
-        alt="background parallax city"
-        class="scale-200 absolute bottom-0 left-0 -translate-y-1/2"
-        style={`transform: translateX(${25 - (scrollLeft / maxScroll) * 50}%)`}
-    />
-</div>
+{#if content}
+    <Parallax scrollContainer={content} />
+{/if}
 
 <div
     class="relative flex h-screen snap-x snap-mandatory flex-row overflow-x-scroll bg-gray-50/50"
