210d4781352e1f61ebaebc4c91357f41c1a880cb
andrewtlu <andrew.lu@emory.edu>
2025-03-07 17:36:25 -0500

    Add initial keyframe info

--- website/src/lib/keyframe.ts
+++ website/src/lib/keyframe.ts
@@ -5,14 +5,15 @@
  */
 export type Keyframe = {
     bolded: (0 | 1 | 2 | 3)[]; // lines to be bolded
-    toRun: Function[];
+    toRun: (() => void)[];
 };
 
 /**
  * Stores a list of keyframes for a specific verse
+ * frames[] must contain at least 1 frame (default)
  */
 export type Keyframes = {
-    frames: Keyframe[];
+    frames: [Keyframe, ...Keyframe[]];
 };
 
 /**
@@ -20,6 +21,10 @@ export type Keyframes = {
  */
 export const verse1Frames: Keyframes = {
     frames: [
+        {
+            bolded: [],
+            toRun: []
+        },
         {
             bolded: [0, 1],
             toRun: []
@@ -35,21 +40,62 @@ export const verse1Frames: Keyframes = {
  * Frames within verse 2
  */
 export const verse2Frames: Keyframes = {
-    frames: []
+    frames: [
+        {
+            bolded: [],
+            toRun: []
+        }
+    ]
 };
 
 /**
  * Frames within verse 3
  */
 export const verse3Frames: Keyframes = {
-    frames: []
+    frames: [
+        {
+            bolded: [],
+            toRun: []
+        }
+    ]
 };
 
 /**
  * Frames within verse 4
  */
 export const verse4Frames: Keyframes = {
-    frames: []
+    frames: [
+        {
+            bolded: [],
+            toRun: []
+        }
+    ]
 };
 
 export const keyframes: Keyframes[] = [verse1Frames, verse2Frames, verse3Frames, verse4Frames];
+
+export function getFramesCount(keyframe: Keyframes) {
+    return keyframe.frames.length;
+}
+
+export function getTotalFramesCount() {
+    let count = 0;
+    keyframes.forEach((frame: Keyframes) => (count += frame.frames.length));
+    return count;
+}
+
+export function getFrame(keyframe: number): Keyframe {
+    let index = keyframe;
+    // traverse frames
+    for (let verseFrames of keyframes) {
+        if (index >= getFramesCount(verseFrames)) {
+            index -= getFramesCount(verseFrames);
+        } else {
+            return verseFrames.frames[index];
+        }
+    }
+
+    throw Error(
+        `Tried accessing keyframe ${keyframe + 1}, but there are only ${getTotalFramesCount()} frames!`
+    );
+}
