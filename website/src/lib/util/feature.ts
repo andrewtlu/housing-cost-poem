8462bc4386a4ca2674a3360240167b6eac11040c
andrewtlu <andrew.lu@emory.edu>
2025-03-23 19:58:46 -0400

    Format

--- website/src/lib/util/feature.ts
+++ website/src/lib/util/feature.ts
@@ -1,29 +1,37 @@
 import type { FeatureCollection, Polygon, MultiPolygon, Feature } from "geojson";
 
-export default function feature(geojsonFeatures: ({
-    type: string;
-    properties: { id: string };
-    geometry: {
-        type: string;
-        coordinates: number[][][];
-    };
-} | {
-    type: string;
-    properties: { id: string };
-    geometry: {
-        type: string;
-        coordinates: number[][][][];
-    };
-})[]): FeatureCollection<Polygon | MultiPolygon> {
+export default function feature(
+    geojsonFeatures: (
+        | {
+              type: string;
+              properties: { id: string };
+              geometry: {
+                  type: string;
+                  coordinates: number[][][];
+              };
+          }
+        | {
+              type: string;
+              properties: { id: string };
+              geometry: {
+                  type: string;
+                  coordinates: number[][][][];
+              };
+          }
+    )[]
+): FeatureCollection<Polygon | MultiPolygon> {
     return {
         type: "FeatureCollection",
-        features: geojsonFeatures.map(feature => ({
-            type: "Feature",
-            properties: feature.properties,
-            geometry: {
-                type: feature.geometry.type as "Polygon" | "MultiPolygon",
-                coordinates: feature.geometry.coordinates,
-            }
-        }) as Feature<Polygon | MultiPolygon>)
-    }
+        features: geojsonFeatures.map(
+            (feature) =>
+                ({
+                    type: "Feature",
+                    properties: feature.properties,
+                    geometry: {
+                        type: feature.geometry.type as "Polygon" | "MultiPolygon",
+                        coordinates: feature.geometry.coordinates
+                    }
+                }) as Feature<Polygon | MultiPolygon>
+        )
+    };
 }
