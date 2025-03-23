200a5381e94030cc705c3b6dab692d0b83155cf1
andrewtlu <andrew.lu@emory.edu>
2025-03-22 20:09:08 -0400

    Style title page

--- website/src/lib/components/title/title.svelte
+++ website/src/lib/components/title/title.svelte
@@ -1,6 +1,34 @@
 <script lang="ts">
 </script>
 
-<div class="flex h-screen w-screen shrink-0 flex-col items-center justify-center">
-    <h1>Affording an Abode: Affordable Housing in Urban America</h1>
+<div class="flex h-screen w-screen shrink-0 flex-col items-center justify-center font-sans">
+    <!-- outer div constrains subtitle and names -->
+    <div>
+        <!-- title -->
+        <div class="flex flex-col items-center font-extrabold">
+            <div class="flex flex-row items-center">
+                <div class="text-9xl">Afford</div>
+                <div class="mb-2 flex flex-col justify-end self-end text-2xl">ing an</div>
+            </div>
+            <div class="ml-46 -mt-5 text-7xl">Abode:</div>
+        </div>
+
+        <div class="flex w-full items-end justify-between">
+            <!-- subtitle -->
+            <div class="order-2 mr-4 flex flex-col items-end text-3xl font-medium leading-7">
+                <div>Affordable</div>
+                <div>Housing</div>
+                <div>in</div>
+                <div>Urban</div>
+                <div>America</div>
+            </div>
+
+            <!-- names -->
+            <div class="order-1 flex flex-col items-start text-xl font-light">
+                <div>Michelle Kim,</div>
+                <div>Jason Lebov,</div>
+                <div>& Andrew Lu</div>
+            </div>
+        </div>
+    </div>
 </div>
