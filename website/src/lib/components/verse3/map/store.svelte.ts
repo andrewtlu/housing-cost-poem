e04617a242b2c9c68cb8daf1022cfbb545eb2bef
andrewtlu <andrew.lu@emory.edu>
2025-04-10 15:14:25 -0400

    Change color to lightgray

--- website/src/lib/components/verse3/map/store.svelte.ts
+++ website/src/lib/components/verse3/map/store.svelte.ts
@@ -30,7 +30,6 @@ export const attributeMap = {
         textValue: (county: County) => county.total_population.toLocaleString("en-US"),
         value: (county: County) => county.total_population,
         color: ["#f9ffba", "red"],
-        mutedColor: ["#f4f6d7", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value / 1000).toLocaleString("en-US")}k`
     },
     total_population_25_over: {
@@ -38,7 +37,6 @@ export const attributeMap = {
         textValue: (county: County) => county.total_population_25_over.toLocaleString("en-US"),
         value: (county: County) => county.total_population_25_over,
         color: ["#f9ffba", "red"],
-        mutedColor: ["#f4f6d7", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value / 1000).toLocaleString("en-US")}k`
     },
     educational_attainment: {
@@ -47,7 +45,6 @@ export const attributeMap = {
             `${Math.round((county.educational_attainment / county.total_population) * 100)}%`,
         value: (county: County) => county.educational_attainment / county.total_population,
         color: ["#f9ffba", "red"],
-        mutedColor: ["#f4f6d7", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value * 100)}%`
     },
     median_income: {
@@ -55,7 +52,6 @@ export const attributeMap = {
         textValue: (county: County) => `$${county.median_income.toLocaleString("en-US")}`,
         value: (county: County) => county.median_income,
         color: ["#f9ffba", "red"],
-        mutedColor: ["#f4f6d7", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value / 1000).toLocaleString("en-US")}k`
     },
     median_home_value: {
@@ -63,7 +59,6 @@ export const attributeMap = {
         textValue: (county: County) => `$${county.median_home_value.toLocaleString("en-US")}`,
         value: (county: County) => county.median_home_value,
         color: ["#f9ffba", "red"],
-        mutedColor: ["#f4f6d7", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value / 1000).toLocaleString("en-US")}k`
     },
     white_alone: {
@@ -72,7 +67,6 @@ export const attributeMap = {
             `${Math.round((county.white_alone / county.total_population) * 100)}%`,
         value: (county: County) => county.white_alone / county.total_population,
         color: ["steelblue", "red"],
-        mutedColor: ["#9facb6", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value * 100)}%`
     },
     black_alone: {
@@ -81,7 +75,6 @@ export const attributeMap = {
             `${Math.round((county.black_alone / county.total_population) * 100)}%`,
         value: (county: County) => county.black_alone / county.total_population,
         color: ["steelblue", "red"],
-        mutedColor: ["#9facb6", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value * 100)}%`
     },
     native_alone: {
@@ -90,7 +83,6 @@ export const attributeMap = {
             `${Math.round((county.native_alone / county.total_population) * 100)}%`,
         value: (county: County) => county.native_alone / county.total_population,
         color: ["steelblue", "red"],
-        mutedColor: ["#9facb6", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value * 100)}%`
     },
     asian_alone: {
@@ -99,7 +91,6 @@ export const attributeMap = {
             `${Math.round((county.asian_alone / county.total_population) * 100)}%`,
         value: (county: County) => county.asian_alone / county.total_population,
         color: ["steelblue", "red"],
-        mutedColor: ["#9facb6", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value * 100)}%`
     },
     native_hawaiian_pacific_islander: {
@@ -109,7 +100,6 @@ export const attributeMap = {
         value: (county: County) =>
             county.native_hawaiian_pacific_islander / county.total_population,
         color: ["steelblue", "red"],
-        mutedColor: ["#9facb6", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value * 100)}%`
     },
     some_other_race_alone: {
@@ -118,7 +108,6 @@ export const attributeMap = {
             `${Math.round((county.some_other_race_alone / county.total_population) * 100)}%`,
         value: (county: County) => county.some_other_race_alone / county.total_population,
         color: ["steelblue", "red"],
-        mutedColor: ["#9facb6", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value * 100)}%`
     },
     two_or_more: {
@@ -127,7 +116,6 @@ export const attributeMap = {
             `${Math.round((county.two_or_more / county.total_population) * 100)}%`,
         value: (county: County) => county.two_or_more / county.total_population,
         color: ["steelblue", "red"],
-        mutedColor: ["#9facb6", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value * 100)}%`
     },
     hispanic_or_latino: {
@@ -136,7 +124,6 @@ export const attributeMap = {
             `${Math.round((county.hispanic_or_latino / county.total_population) * 100)}%`,
         value: (county: County) => county.hispanic_or_latino / county.total_population,
         color: ["steelblue", "red"],
-        mutedColor: ["#9facb6", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value * 100)}%`
     },
     total_population_25_under: {
@@ -144,7 +131,6 @@ export const attributeMap = {
         textValue: (county: County) => county.total_population_25_under.toLocaleString("en-US"),
         value: (county: County) => county.total_population_25_under,
         color: ["#f9ffba", "red"],
-        mutedColor: ["#f4f6d7", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value / 1000).toLocaleString("en-US")}k`
     },
     proportion_25_under: {
@@ -152,7 +138,6 @@ export const attributeMap = {
         textValue: (county: County) => `${Math.round(county.proportion_25_under * 100)}%`,
         value: (county: County) => county.proportion_25_under,
         color: ["#f9ffba", "red"],
-        mutedColor: ["#f4f6d7", "#ff8d8d"],
         tickFormat: (value: number) => `${Math.round(value * 100)}%`
     }
 };
@@ -170,13 +155,6 @@ export const cluster_colors = $state([
     scaleLinear<string>(),
     scaleLinear<string>()
 ]);
-export const muted_cluster_colors = $state([
-    scaleLinear<string>(),
-    scaleLinear<string>(),
-    scaleLinear<string>(),
-    scaleLinear<string>(),
-    scaleLinear<string>()
-]);
 let cluster_ranges: number[][] = [[], [], [], [], []];
 export const getColor = (county: Feature<Geometry, GeoJsonProperties>, centroid: number = -1) => {
     const county_data = data.get(county.id as number);
@@ -185,10 +163,7 @@ export const getColor = (county: Feature<Geometry, GeoJsonProperties>, centroid:
             return cluster_colors[county_data.area_cluster](
                 attributeMap[attribute].value(county_data)
             );
-        else
-            return muted_cluster_colors[county_data.area_cluster](
-                attributeMap[attribute].value(county_data)
-            );
+        else return "lightgray";
     }
     return "white";
 };
@@ -203,8 +178,5 @@ export const reloadColors = () => {
         cluster_colors[idx] = scaleLinear<string>()
             .domain(extent(range) as [number, number])
             .range(attributeMap[attribute].color);
-        muted_cluster_colors[idx] = scaleLinear<string>()
-            .domain(extent(range) as [number, number])
-            .range(attributeMap[attribute].mutedColor);
     });
 };
