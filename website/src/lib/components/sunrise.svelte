dfcecacb2cc0f2a27ac9766bbd9e186a86d10ef1
andrewtlu <andrew.lu@emory.edu>
2025-04-13 17:28:49 -0400

    Finish sunrise

--- website/src/lib/components/sunrise.svelte
+++ website/src/lib/components/sunrise.svelte
@@ -15,7 +15,6 @@
     let xPos = $state(0);
     let yPos = $state(0);
     let sunMoonRadius = $state(0);
-    let rotate = $state(60);
     const transitionStyle = "transition: all .5s ease;";
     const sunRadius = 40;
     const moonRadius = 38;
@@ -30,7 +29,7 @@
             .domain([0, nonEndingFrameCount - 1])
             .range([0, 1])
     );
-    let sunUp = $state(interpolateRgbBasis(["#030436", "#43afca", "#a6e1fa"]));
+    let sunUp = $state(interpolateRgbBasis(["#43afca", "#a6e1fa"]));
     const sunUpScale = $state(
         scaleLinear()
             .domain([nonEndingFrameCount, totalFrames - 1])
@@ -42,15 +41,32 @@
     });
 
     // sun-moon rotation
+    const nightRotateScale = $state(
+        scaleLinear()
+            .domain([0, nonEndingFrameCount - 1])
+            .range([60, 160])
+    );
+    const dayRotateScale = $state(
+        scaleLinear()
+            .domain([nonEndingFrameCount, totalFrames - 1])
+            .range([180, 240])
+    );
+    let rotate = $derived.by(() => {
+        if (keyframe.value < nonEndingFrameCount) return nightRotateScale(keyframe.value);
+        else return dayRotateScale(keyframe.value);
+    });
 
-    // refraction optical illusion size
+    // grow sun on sunrise
     const size = $state(
         scalePow()
             .domain([0, totalFrames - 1])
             .range([1, 2.5])
             .exponent(5)
     );
-    let transformScale = $derived(size(keyframe.value));
+    let transformScale = $derived.by(() => {
+        if (keyframe.value < nonEndingFrameCount) return 0;
+        else return size(keyframe.value);
+    });
 
     $effect(() => {
         if (container) {
@@ -65,7 +81,6 @@
                 styles.getPropertyValue("--color-midnight")
             ]);
             sunUp = interpolateRgbBasis([
-                styles.getPropertyValue("--color-midnight"),
                 styles.getPropertyValue("--color-dawn"),
                 styles.getPropertyValue("--color-sunrise")
             ]);
@@ -82,15 +97,15 @@
 
         <!-- if statement prevents circle spawning in top left corner on page load -->
         {#if container}
-            <!-- inline style for rotating around center, see https://stackoverflow.com/questions/15138801/rotate-rectangle-around-its-own-center-in-svg -->
             <g
                 transform={`translate(${xPos}, ${yPos}) rotate(${rotate})`}
-                transform-origin="center"
-                style={`${transitionStyle} transform-box: fill-box;`}
+                style={transitionStyle}
+                width={sunMoonRadius * 2}
+                height={sunRadius}
             >
                 <!-- sun -->
                 <g
-                    transform={`scale(${transformScale}) translate(${sunMoonRadius}, 0)`}
+                    transform={`scale(${transformScale}) translate(${sunMoonRadius / 3}, 0)`}
                     style={transitionStyle}
                 >
                     <circle fill="var(--color-sun-outer)" cx="0" cy="0" r={sunRadius * 3}> </circle>
@@ -99,7 +114,10 @@
                 </g>
 
                 <!-- moon -->
-                <g transform={`translate(-${sunMoonRadius}, 0)`} style={transitionStyle}>
+                <g
+                    transform={`translate(-${sunMoonRadius}, 0) ${keyframe.value >= nonEndingFrameCount ? "scale(0)" : "scale(1)"}`}
+                    style={transitionStyle}
+                >
                     <defs>
                         <mask id="moon">
                             <circle fill="white" cx="0" cy="0" r={moonRadius}> </circle>
@@ -118,6 +136,7 @@
                         cy="0"
                         r={moonRadius}
                         mask="url(#moon)"
+                        style={transitionStyle}
                         transform={`rotate(-${rotate})`}
                     >
                     </circle>
